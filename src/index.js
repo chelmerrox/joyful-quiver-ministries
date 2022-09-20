1390d8116b46e56d702187fb44f4ffee87264eea
Losalini <c.rokocakau@gmail.com>
2022-09-21 08:14:30 +1200

    Add navbar for mobile, tablet & desktop screens

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+/* Variable Declarations */
+const hamburger = document.querySelector('.hamburger');
+const navMenuMobile = document.querySelector('.nav-menu-mobile');
+
+/* Function Declarations */
+function openMenu() {
+  hamburger.classList.toggle('active');
+  navMenuMobile.classList.toggle('active');
+}
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenuMobile.classList.remove('active');
+}
+
+// Open Nav Menu for touchstart
+hamburger.addEventListener('touchstart', openMenu);
+
+// Close Nav Menu for touchend
+document.querySelectorAll('.nav-link-mobile').forEach((n) => n.addEventListener('touchend', closeMenu))
