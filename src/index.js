129e9e776610928950767f5fb4944c5085c1afa4
Losalini <c.rokocakau@gmail.com>
2022-09-26 19:48:35 +1200

    Edit About Us button in navbar for desktop screens

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import './style.css';
 import { hamburger, openMenu, closeMenu } from './modules/mobile-menu.js';
+// import BackgroundImg1 from './images/background-1-1-1.png';
 
-// Open Nav Menu for touchstart
+/* Variable declarations */
+const sectionOne = document.querySelector('.section-1');
+const paragraphOne = document.querySelector('.paragraph-1');
+const beFreeText = document.querySelector('.be-free-text');
+
+// Open Nav Menu for click
 hamburger.addEventListener('click', openMenu);
 
-// Close Nav Menu for touchend
+// Close Nav Menu for click
 document.querySelectorAll('.nav-link-mobile').forEach((n) => n.addEventListener('click', closeMenu));
+
+const navMenuMobileLinks = Array.from(document.getElementsByClassName('nav-link-mobile'));
+
+navMenuMobileLinks.forEach((link, i) => {
+  link.addEventListener('click', (e) => {
+    if (i === 0){
+      sectionOne.style.backgroundImage = 'url("./images/background-1-1-1.png")';
+      sectionOne.backgroundPosition = 'center';
+      sectionOne.backgroundRepeat = 'no-repeat';
+      sectionOne.backgroundSize =  'contain';
+      paragraphOne.classList.add('active');
+      beFreeText.classList.add('active');
+
+    } else if (i === 1) {
+      sectionOne.style.backgroundImage = 'url("./images/background-2.png")';
+      paragraphOne.classList.remove('active');
+      beFreeText.classList.remove('active');
+    } else {
+      
+    }
+  });
+});
---
+++ src/modules/booking-section.js
@@ -0,0 +1,4 @@
+/* Variable declarations */
+const sectionOne = document.querySelector('.section-one');
+
+const navMenuMobileLinks = Array.from(document.getElementsByClassName('.nav-menu-mobile .nav-item-mobile .nav-link-mobile'));
